975d5566e47c32024536a95665401b210ed4affa
Lina Maria Montaño Ramirez | calypso Bronte <codelinamaria@gmail.com>
2018-11-26 23:31:35 -0500

    challenges 1 🤓

--- challenges/2.js
+++ challenges/2.js
@@ -6,5 +6,13 @@ siete y tres en el código.
 
 let siete = 7;
 let tres = "3";
-console.log(siete + tres);
+console.log(siete + tres); //Este concatena number con string pero este no se suman por que si fuera asi seria 10 su resultrado pero este imprime 73
 // Tu código va aquí
+/*
+    agregamos typeof para mirar de que tipo es la variable que estamos haciendo.
+    console.log(typeof tres);
+    console.log(typeof siete);
+        
+*/
+console.log(typeof tres);
+console.log(typeof siete);
