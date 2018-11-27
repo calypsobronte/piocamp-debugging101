4f07ffda7fafcb3556de2bb31ce4eb4419e00c9c
Lina Maria Montaño Ramirez | calypso Bronte <codelinamaria@gmail.com>
2018-11-26 23:19:47 -0500

    challenges 1 🤓

--- challenges/1.js
+++ challenges/1.js
@@ -7,6 +7,15 @@ let a = 5;
 let b = 1;
 a++;
 // Tu código va aquí
+console.log('Resultado de a es:', a);
 
+
+/*Agregamos 
+a= 5; si lo juntamos este incrementa (++a) en 1 por lo tanto el resultado es 6
+console.log('Resultado de a es:', a);
+console.log('Resultado de sumAB es:', sumAB);
+
+ */
 let sumAB = a + b;
-console.log(sumAB);
+// Tu código va aquí
+console.log('Resultado de sumAB es:', sumAB);
