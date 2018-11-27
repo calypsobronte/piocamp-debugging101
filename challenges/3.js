702385a762a95dbc01c8b0077bd929beefd00bb7
Lina Maria Montaño Ramirez | calypso Bronte <codelinamaria@gmail.com>
2018-11-26 23:40:49 -0500

    challenges 3 🤓

--- challenges/3.js
+++ challenges/3.js
@@ -5,5 +5,19 @@ netWorkingCapital.
 
 let receivables = 10;
 let payables = 8;
-let netWorkingCapital = recievables - payable;
+// Codigo incorrecto
+// let netWorkingCapital = recievables - payable;
+
+//Codigo correcto
+let netWorkingCapital = receivables - payables;
+
 console.log(`Net working capital is: ${netWorkingCapital}`);
+/*
+    Aca vamos a corregir un error de tipeo en el cual muchos no nos percatamos y suele atrancarnos a la hora de programar
+    es como escribimos nuestro codigo 
+    Variables malas 
+    recievables - payable;
+    Corregidas
+    receivables - payables
+
+*/
